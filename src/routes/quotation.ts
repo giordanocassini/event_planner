76996e63145cbeb807ac1ab9c96d535ecc5738a8
giordano.cassini <giordano.cassini@gmail.com>
2023-09-17 19:30:31 -0300

    fix: list expense functions working

--- src/routes/quotation.ts
+++ src/routes/quotation.ts
@@ -1,20 +1,18 @@
-import { QuotationController } from "./../controllers/QuotationController";
-import { Router } from "express";
-import { checkJwt } from "../middlewares/auth/checkJwt";
+import { QuotationController } from './../controllers/QuotationController';
+import { Router } from 'express';
+import { checkJwt } from '../middlewares/auth/checkJwt';
 import { findEventById } from './../middlewares/dbService/findEventById';
 
 const router = Router();
 
-router.route("/quotation/:eventId([0-9]+)").post([checkJwt], [findEventById], QuotationController.createQuotation);
+router.route('/quotation/:eventId([0-9]+)')
+.post([checkJwt], [findEventById], QuotationController.createQuotation);
 
-router
-  .route("/quotation/:id([0-9]+)")
-  .get([checkJwt],QuotationController.getQuotationById)
-  .put([checkJwt],QuotationController.editQuotation)
-  .delete([checkJwt],QuotationController.deleteQuotation);
+router.route('/quotation/:id([0-9]+)')
+.get([checkJwt], QuotationController.getQuotationById)
+.put([checkJwt], QuotationController.editQuotation)
+.delete([checkJwt], QuotationController.deleteQuotation);
 
-router
-  .route("/quotation/event/:id([0-9]+)")
-  .get([checkJwt],QuotationController.getAllQuotationByEventId);
+router.route('/quotation/event/:id([0-9]+)').get([checkJwt], QuotationController.getAllQuotationByEventId);
 
 export default router;
