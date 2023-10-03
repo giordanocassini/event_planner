f6f1150e70761658b45b4942c371a399b6acebf5
giordano.cassini <giordano.cassini@gmail.com>
2023-10-03 15:49:18 -0300

    feat: mock working, first test goes ok

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,19 +1,23 @@
 import request from 'supertest';
 import app from '../app'; // Adjust the path to your Express app
 import UserDbService from '../services/UserDbService';
-
+import { AuthController } from '../controllers/AuthController';
 const userDbService: UserDbService = UserDbService.getInstance();
 
 describe('Authentication Route Tests', () => {
   const userPayload = {
-    user: {},
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjk2Mjc3NTM0LCJleHAiOjE2O',
+    email: 'joazinho2@email.com',
+    password: 'hash',
   };
 
-  const userInput = { email: 'joazinho2@email.com', password: "123456" };
+  const userInput = { email: 'joazinho2@email.com', password: '123456' };
 
   it('should return 200 and a token for valid login', async () => {
-    const createUserServiceMock = jest
+    const checkIfUnencryptedPasswordIsValidMock = jest.spyOn(AuthController, 'checkIfUnencryptedPasswordIsValid').mockImplementation((password, hashedPassword) => {
+      return password === '123456' && hashedPassword === 'hash';
+    });
+
+    const createUserDbServiceMock = jest
       .spyOn(userDbService, 'findByEmail')
       // @ts-ignore
       .mockReturnValueOnce(userPayload);
@@ -22,6 +26,6 @@ describe('Authentication Route Tests', () => {
 
     expect(response.status).toEqual(200);
     expect(response.body).toHaveProperty('token');
-    expect(createUserServiceMock).toHaveBeenCalledWith(userInput);
+    expect(response.body).toHaveProperty('user');
   });
 });
--- src/middlewares/validations/checkPassword.ts
+++ src/middlewares/validations/checkPassword.ts
@@ -1,14 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const checkPassword = (req: Request, res: Response, next: NextFunction) => {
-  console.log('here');
-  
   const { password } = req.body;
-  console.log("🚀 ~ file: checkPassword.ts:5 ~ checkPassword ~ password:", password)
 
   if (typeof password != 'string') {
     return res.status(400).send('Invalid type of parameters on request');
   }
-  
+
   next();
 };
